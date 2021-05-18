943ca83426364f1fdfd9ea702d441075110dbf6f
Lu Zhihao <lu_zhihao@u.nus.edu>
2021-05-18 18:38:47 +0800

    Add function and display for overall win rate

--- app.R
+++ app.R
@@ -2,25 +2,52 @@ library(tidyverse)
 library(lubridate)
 library(googlesheets4)
 library(shiny)
-ui <- fluidPage(
-  checkboxGroupInput(inputId = "maptype",label="Map Type",
-                     choices = c("gemGrab","bounty","heist","brawlBall","hotZone","siege")
-  ),
-  checkboxGroupInput(inputId = "gametype",label="Game Type",
-                     choices = c("soloRanked", "teamRanked")
-  ),
-  plotOutput("plot")
-)
+library(shinydashboard)
+
+ui <- dashboardPage(dashboardHeader(),
+                    dashboardSidebar(),
+                    dashboardBody(
+                     valueBoxOutput("winRateBox")))
+                      
+                   
 
 server <- function(input, output) {
   #options(gargle_oauth_cache = ".secrets")
   #gs4_auth()
-  options(
-    gargle_oauth_cache = ".secrets",
-    gargle_oauth_email = TRUE
-  )
-  data <- read_sheet("https://docs.google.com/spreadsheets/d/1-b6HCSTrTdS12dLWhqq3VzWGPrWf9sbN1r1L4vWNzAI/edit#gid=2008828887", sheet = "Sheet3")
-  print("hi")
+  options(gargle_oauth_cache = ".secrets",
+          gargle_oauth_email = TRUE)
+  data <-
+    read_sheet(
+      "https://docs.google.com/spreadsheets/d/1-b6HCSTrTdS12dLWhqq3VzWGPrWf9sbN1r1L4vWNzAI/edit#gid=2008828887",
+      sheet = "Sheet3"
+    )
+  
+  countWins <- function(data, event, mode) {
+    if (!missing(event)) {
+      data <- filter(data, Event = event)
+    }
+    
+    if (!missing(mode)) {
+      data <- filter(data, Mode = mode)
+    }
+    
+    output <- data  %>%
+      mutate(value = (Result == "victory")) %>%
+      group_by(SD) %>%
+      summarise(a = sum(sum(value) > 1))
+    return(output)
+  }
+  
+  winRate <- round(mean(countWins(data)$a) * 100,2)
+  print(winRate)
+  
+  output$winRateBox <- renderValueBox({
+    valueBox(
+      paste0(winRate,"%"), paste0("Win Rate out of ", nrow(countWins(data))," games played"), icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
   data_new <- mutate(data, day = date(ymd_hms(DateTime)))
   #data_filter <- filter(data, Event == input$maptype)
   output$plot <- renderPlot({
