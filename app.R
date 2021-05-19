401251ab7bd692747dd3fe3441fa03eb929d6a32
Lu Zhihao <lu_zhihao@u.nus.edu>
2021-05-20 03:03:25 +0800

    Add date filtering to overview tab

--- app.R
+++ app.R
@@ -91,7 +91,7 @@ server <- function(input, output) {
   
   data <- reactive({
 
-    filter(data, between(ymd_hms(DateTime), now()-days(input$dateFilter), now()))
+    dplyr::filter(raw_data, between(ymd_hms(DateTime), now()-days(input$dateFilter), now()))
   
   })
   
@@ -99,22 +99,23 @@ server <- function(input, output) {
   badgeURL <- "https://cdn.brawlstats.com/ranked-ranks/ranked_ranks_l_"
   
   
-  soloData <- filter(data(), Mode == "soloRanked")
-  teamData <- filter(data(), Mode == "teamRanked")
+  soloData <- reactive({filter(data(), Mode == "soloRanked")})
+  teamData <- reactive({filter(data(), Mode == "teamRanked")})
   
   
   
   #current rank for each mode
-  soloRank <- soloData$MeRank[[nrow(soloData)]]
-  teamRank <- teamData$MeRank[[nrow(teamData)]]
-  bestRank <- max(soloRank, teamRank)
+  soloRank <- reactive({soloData()$MeRank[[nrow(soloData())]]})
+  teamRank <- reactive({teamData()$MeRank[[nrow(teamData())]]})
+  bestRank <- reactive({max(soloRank(), teamRank())})
   
-  soloBadgeURL <- paste0(badgeURL, soloRank, ".png")
-  teamBadgeURL <- paste0(badgeURL, teamRank, ".png")
-  bestBadgeURL <- paste0(badgeURL, bestRank, ".png")
+  soloBadgeURL <- reactive({paste0(badgeURL, soloRank(), ".png")})
+  teamBadgeURL <- reactive({paste0(badgeURL, teamRank(), ".png")})
+  bestBadgeURL <- reactive({paste0(badgeURL, bestRank(), ".png")})
   
   #function to count win rate
   countWins <- function(data, event, mode) {
+    data <- data()
     if (!missing(event)) {
       data <- filter(data, Event == event)
     }
@@ -136,103 +137,103 @@ server <- function(input, output) {
     return(out)
   }
   
-  winRate <- round(mean(countWins(data())$a) * 100,2)
-  print(winRate)
-  output$winRateBox <- renderValueBox({
-    valueBox(
-      div(paste0(winRate,"%"), tags$img(src = bestBadgeURL, height="100px", width="200px")), paste0("Win rate out of ", nrow(countWins(data()))," games played"),
-      color = "purple"
-    )
-  })
+   winRate <- reactive({round(mean(countWins(data())$a) * 100,2)})
   
-  soloWinRate <- round(mean(countWins(data(), mode = "soloRanked")$a) * 100,2)
-  output$soloWinRateBox <- renderValueBox({
-    valueBox(
-      div(paste0(soloWinRate,"%"), tags$img(src = soloBadgeURL, height="100px", width="200px")), paste0("Solo win rate out of ", nrow(countWins(data(), mode = "soloRanked"))," games played"),
-      color = "blue", icon = 
-    )
-  })
-
-  teamWinRate <- round(mean(countWins(data(), mode = "teamRanked")$a) * 100,2)
-  output$teamWinRateBox <- renderValueBox({
-    valueBox(
-      div(paste0(teamWinRate,"%"), tags$img(src = teamBadgeURL, height="100px", width="200px")), paste0("Team win rate out of ", nrow(countWins(data(), mode = "teamRanked"))," games played"),
-      color = "red"
-    )
-  })
+   output$winRateBox <- renderValueBox({
+     valueBox(
+       div(paste0(winRate(),"%"), tags$img(src = bestBadgeURL(), height="100px", width="200px")), paste0("Win rate out of ", nrow(countWins(data()))," games played"),
+       color = "purple"
+     )
+   })
   
+   soloWinRate <- reactive({round(mean(countWins(data(), mode = "soloRanked")$a) * 100,2)})
+   output$soloWinRateBox <- renderValueBox({
+     valueBox(
+       div(paste0(soloWinRate(),"%"), tags$img(src = soloBadgeURL(), height="100px", width="200px")), paste0("Solo win rate out of ", nrow(countWins(data(), mode = "soloRanked"))," games played"),
+       color = "blue", icon =
+     )
+   })
   
-# Win rate chart
-  plotData <- inner_join(countWins(data()), select(data(),Event, Mode, SD), by = "SD") %>% 
-    distinct() %>% 
-    mutate(Status = ifelse(a == 1, "win","lose"))
-  
-  output$packagePlot <- renderPlot({
-    ggplot(plotData) +
-    geom_bar(aes(x = Event, fill = Status)) +
-    facet_wrap(~Mode)
-  })
-  
-  
-# Table for mode  
-  # output$modeTable <- renderTable({
-  #   data() %>% 
-  #   group_by(Event, Mode) %>% 
-  #   summarise("Win Rate" = mean(countWins(data(), Event, Mode)$a), "Games Played" = nrow(countWins(data(), Event, mode = Mode ))) %>%
-  #   mutate("Win Rate" = paste0(round(`Win Rate`,2), " (",`Games Played`,")")) %>%
-  #   select(1:3) %>%
-  #   spread(Mode, `Win Rate`) %>%
-  #   arrange(desc(teamRanked))
-  # })
-
-
-  #My best brawler progress bar
-  output$brawlerProgress <- renderUI({
-    bb <- inner_join(countWins(data()), select(data(),Me, Mode, SD), by = "SD") %>% 
-      distinct() %>% 
-      group_by(Me) %>% 
-      summarise("Win%" = mean(a), "Count" = n()) %>%
-      mutate("Win%" = round(`Win%`*100,2)) %>%
-      filter(Count > 1) %>%
-      slice_max(`Win%`, n=5)
-    
-    
-    
-    tags$div(
-      map(seq_len(min(5, nrow(bb))), ~ {
-        progressGroup(paste0(bb$Me[.], "(",bb$Count[.],")"), bb$`Win%`[.], max = 100, color = BASIC_COLORS[.])
-      })
-    )
-  })
+   teamWinRate <- reactive({round(mean(countWins(data(), mode = "teamRanked")$a) * 100,2)})
+   output$teamWinRateBox <- renderValueBox({
+     valueBox(
+       div(paste0(teamWinRate(),"%"), tags$img(src = teamBadgeURL(), height="100px", width="200px")), paste0("Team win rate out of ", nrow(countWins(data(), mode = "teamRanked"))," games played"),
+       color = "red"
+     )
+   })
   
 
+ # Win rate chart
+   plotData <- reactive({inner_join(countWins(data()), select(data(),Event, Mode, SD), by = "SD") %>%
+     distinct() %>%
+     mutate(Status = ifelse(a == 1, "win","lose"))})
+
+   output$packagePlot <- renderPlot({
+     ggplot(plotData()) +
+     geom_bar(aes(x = Event, fill = Status)) +
+     facet_wrap(~Mode)
+   })
+
+
+# Table for mode
+  output$modeTable <- renderTable({
+    data() %>%
+     group_by(Event, Mode) %>%
+     summarise("Win Rate" = mean(countWins(data(), Event, Mode)$a), "Games Played" = nrow(countWins(data(), Event, mode = Mode ))) %>%
+     mutate("Win Rate" = paste0(round(`Win Rate`,2), " (",`Games Played`,")")) %>%
+     select(1:3) %>%
+     spread(Mode, `Win Rate`) %>%
+     arrange(desc(teamRanked))
+   })
+
+
+   #My best brawler progress bar
+   output$brawlerProgress <- renderUI({
+     bb <- inner_join(countWins(data()), select(data(),Me, Mode, SD), by = "SD") %>%
+       distinct() %>%
+       group_by(Me) %>%
+       summarise("Win%" = mean(a), "Count" = n()) %>%
+       mutate("Win%" = round(`Win%`*100,2)) %>%
+       filter(Count > 1) %>%
+       slice_max(`Win%`, n=5)
   
-  #My best map table
-  output$mapProgress <- renderUI({
-    bm <- inner_join(countWins(data()), select(data(),Map, Mode, SD, Event), by = "SD") %>% 
-      distinct() %>% 
-      group_by(Map) %>% 
-      summarise("Win%" = mean(a), "Count" = n(), Event) %>%
-      mutate("Win%" = round(`Win%`*100,2)) %>%
-      distinct() %>%
-      filter(Count > 1) %>%
-      ungroup %>%
-      slice_max(`Win%`, n=5) 
-    
-    tags$div(
-      map(seq_len(min(5, nrow(bm))), ~ {
-        progressGroup(paste0(bm$Map[.], "(",bm$Count[.],")"), bm$`Win%`[.], max = 100, color = BASIC_COLORS[.])
-      })
-    )
-  })
   
   
+     tags$div(
+       map(seq_len(min(5, nrow(bb))), ~ {
+         progressGroup(paste0(bb$Me[.], "(",bb$Count[.],")"), bb$`Win%`[.], max = 100, color = BASIC_COLORS[.])
+       })
+     )
+   })
+
+
+
+   #My best map table
+   output$mapProgress <- renderUI({
+     bm <- inner_join(countWins(data()), select(data(),Map, Mode, SD, Event), by = "SD") %>%
+       distinct() %>%
+       group_by(Map) %>%
+       summarise("Win%" = mean(a), "Count" = n(), Event) %>%
+       mutate("Win%" = round(`Win%`*100,2)) %>%
+       distinct() %>%
+       filter(Count > 1) %>%
+       ungroup %>%
+       slice_max(`Win%`, n=5)
+  
+     tags$div(
+       map(seq_len(min(5, nrow(bm))), ~ {
+         progressGroup(paste0(bm$Map[.], "(",bm$Count[.],")"), bm$`Win%`[.], max = 100, color = BASIC_COLORS[.])
+       })
+     )
+   })
+
+
   
   ##### maps tab
   
   # maps dropdown controls
   output$mapList <- renderUI({
-    maps <- data %>% 
+    maps <- raw_data %>% 
       select(Event, Map) %>% 
       filter(Event == input$mapMode) %>% 
       distinct() %>% 
@@ -246,7 +247,7 @@ server <- function(input, output) {
     return(str_remove_all(tag, "\\(.+\\)"))
   }
   
-  mapData <- data %>%
+  mapData <- raw_data %>%
               mutate(Friend2 = tagStrip(Friend2), Friend3 = tagStrip(Friend3)) %>%
               rowwise() %>%
               mutate(set = paste0(sort(c(Me, Friend2, Friend3)), collapse = ','))
@@ -260,12 +261,12 @@ server <- function(input, output) {
   #fullCountWins(data %>% gather(key = "Role", value = "Brawler", 5,7,9,11,13,15), countWins(data)) %>% select(-DateTime) %>% distinct()
   #test1 %>% group_by(Brawler) %>% filter(Event == "bounty") %>% summarise(win = mean(a), n())
   
-  data_new <- mutate(data, day = date(ymd_hms(DateTime)))
-  #data_filter <- filter(data, Event == input$maptype)
-  output$plot <- renderPlot({
-    ggplot(filter(data_new, Event == input$maptype, Mode == input$gametype)) +
-      geom_bar(aes(x = day, fill = Result))
-  })
+  # data_new <- mutate(data, day = date(ymd_hms(DateTime)))
+  # #data_filter <- filter(data, Event == input$maptype)
+  # output$plot <- renderPlot({
+  #   ggplot(filter(data_new, Event == input$maptype, Mode == input$gametype)) +
+  #     geom_bar(aes(x = day, fill = Result))
+  # })
 }
 
 shinyApp(ui = ui, server = server)
